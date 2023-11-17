cb561e45c519e13fad380dca73c2b42be3dde5a0
DestregaSuhendar <destregasuhendar@gmail.com>
2023-11-17 22:35:00 +0700

    first commit

--- UTS/Pengelasan.cpp
+++ UTS/Pengelasan.cpp
@@ -1,28 +1,66 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <windows.h>
 
 int main () {
-	int KP[] = {6,8,12,18,24,30,36,42,49,50};
-	int waktu[] = {1,2,3,4,5,6,7,8,9};
-	int JK = sizeof(KP) / sizeof(KP[0]);
-	printf("Tombol start ditekan. Motor listrik utama aktif dan berputar 180 derajat.\n");
+ char ulangi = 'y';
 	
-	for (int i =0; i < JK; i++) {
+
 	
-		int nilai_tengah = KP[i] / 2;
-		
-		printf("Masukan ketebalan pengelasan secara manual pada array dan hitung nilai tengahnya: ");
-		scanf("%d",&nilai_tengah);
-		printf("Motor listrik utama berputar dan pindah ke nilai tengah %d.\n",nilai_tengah);
-		sleep(1*2);
-		printf("Motor listrik lengan tungkai berputar 45 derajat.\n");
-		sleep(1*2);
-		printf("motor listrik jari robot berputar mendekati objek yang akan di las.\n");
-		sleep(1*2);
-		printf("Proses pengelasan untuk ketebalan %d selama %d detik.\n",KP[i],waktu[i]);
-		sleep(1*2);
-		printf("Pengelasan selesai.Proses akan berulang ke langkah 3.\n");
-    } 
+	int m,c,c2,c3,c4,wp,kp,nt;
 	
-	return 0;
+	Sleep(1000);
+	for (c=0;c<182;c=c+5) {
+	system("cls");
+	printf("Tombol Start Ditekan");	
+	printf("\nMotor Listrik Utama Aktif Dan Berputar %i derajat",c);
+	}	
+	while (kp =! 0){
+	
+	printf("\nMasukan Ketebalan pengelasan Las (mm) : ");
+	scanf("%i",&kp);
+	
+	nt=kp/2;
+	printf("Maka Nilai Tengah : %i",nt);
+	Sleep(1000);
+	printf("\nMotor Listrik Utama Berputar dan Berpindah Pada Salah Satu Titik Nilai Tengah %i ",nt);
+	Sleep(1000);
+	for (c2=0;c2<46;c2++) {
+	system("cls");	
+	printf("Tombol Start Ditekan");	
+	printf("\nMotor Listrik Utama Aktif Dan Berputar %i derajat",c-5);
+	printf("\nMasukan Ketebalan pengelasan Las (mm) : %i",kp);
+	printf("\nMaka Nilai Tengah : %i ",nt);
+	printf("\nMotor Listrik Utama Berputar dan Berpindah Pada Salah Satu Titik Nilai Tengah %i ",nt);
+	printf("\nMotor Listrik Lengan Tunggkai Aktif Dan Berputar %i derajat",c2);	
+	}
+	printf("\nMotor Listrik jari Robot Berputar mendekati Objek");
+	Sleep(1000);
+	
+	wp=nt/2;
+	if (kp == 24) {
+		wp = wp - 2;
+	} 
+	if (kp == 30) {
+		wp = wp - 2;
+	} 
+	if (kp == 36) {
+		wp = wp - 3;
+	} 
+	if (kp == 42) {
+		wp = wp - 3;
+	} 
+	if (kp == 49) {
+		wp = wp - 4;
+	} 
+	if (kp == 50) {
+		wp = wp - 3;
+	} 
+	
+	for (c4=wp;c4>0;c4--) {
+	printf("\nRobot Sedang mengelas dengan ekstimasi waktu %i detik",c4);	
+	Sleep(1000);
+	}
+	printf("\nProses las Selesai");
+	}
+	return 1;
 }
